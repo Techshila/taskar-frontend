1c39b2b8a9542e35e15f2d1dec553ae51128caeb
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-04-09 21:40:18 +0530

    feat: api mappings
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

---
+++ src/services/api/medicine/medicine.ts
@@ -0,0 +1,11 @@
+import type { AxiosResponse } from 'axios';
+import API_ENDPOINTS from '@/services/apiEndpoints';
+import { basicAxios } from '@/services/basicAxios';
+import { _MEDICINE_DETAILS } from '@/types';
+
+export const getCandidates = async (medicineId: string) => {
+  const response: AxiosResponse<_MEDICINE_DETAILS[]> = await basicAxios(
+    API_ENDPOINTS.MEDICINE_DETAIL(medicineId)
+  );
+  return response.data;
+};
