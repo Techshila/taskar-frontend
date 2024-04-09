21afbff2b247d2d7df6b4c0affb2d6bf80bb6052
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-04-09 21:39:53 +0530

    chore: adds dummy medicine data
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,10 @@
+import { _MEDICINE } from '@/types';
+
+export const MEDICINE: _MEDICINE = {
+  id: 123,
+  image:
+    'https://images.unsplash.com/photo-1618120508902-c8d05e7985ee?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  name: 'Pure Vitamin-C Serum',
+  category: 'Vitamin',
+  price: 988,
+};
